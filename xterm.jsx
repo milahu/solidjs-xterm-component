a43b634ebab57ff26f62a9678697f70f71ea3873
Milan Hauth <milahu@gmail.com>
2022-09-13 18:16:32 +0200

    fix ref undefined in production build

--- xterm.jsx
+++ xterm.jsx
@@ -9,14 +9,34 @@ export function Xterm(props) {
   let terminal;
 
   onMount(async () => {
-    terminal = new Terminal();
-    terminal.open(ref);
-    if (props.onLoaded) props.onLoaded(terminal);
+    terminal = new Terminal(props.options);
+
+    // setTimeout is a workaround for
+    // Error: Terminal requires a parent element.
+    // see solidjs issues #116 and #36
+    if (false) {
+      //console.log('Xterm.onMount.setTimeout: ref', ref);
+      terminal.open(ref);
+      if (props.onTerminal) props.onTerminal(terminal);
+    } else {
+      setTimeout(() => {
+        //console.log('Xterm.onMount.setTimeout: ref', ref);
+        terminal.open(ref);
+        if (props.onTerminal) props.onTerminal(terminal);
+      });
+    }
   });
 
   onCleanup(() => {
     terminal.clear();
   });
 
-  return <div ref={ref} />
+  // style for the wrapper div
+  // you probably want to set
+  // either { height: '100%' } in a display:block container
+  // or { 'flex-grow': 1 } in a display:flex container
+
+  // FIXME make it work with xterm-addon-fit
+
+  return <div style={props.style} ref={ref} />
 }
